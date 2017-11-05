40541a54f8824d0cc631dac8e327b7fbe972ee69
João Gradim <gradim@lifeonmars.pt>
2017-11-05 20:11:34 +0000

    Remove pods

--- TemposBotMenubar/Pods/Target Support Files/Pods-TemposBotMenubarTests/Pods-TemposBotMenubarTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TemposBotMenubarTests : NSObject
-@end
-@implementation PodsDummy_Pods_TemposBotMenubarTests
-@end
--- TemposBotMenubar/Pods/Target Support Files/Pods-TemposBotMenubarUITests/Pods-TemposBotMenubarUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TemposBotMenubarUITests : NSObject
-@end
-@implementation PodsDummy_Pods_TemposBotMenubarUITests
-@end
